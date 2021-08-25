962bf55f0bda6669f33a054c913fa12791944643
gabrielborbaweb <59452752+gabrielborbaweb@users.noreply.github.com>
2021-08-25 17:28:29 +0000

    Deploying to gh-pages from @ gabrielborbaweb/carbonlab-projects@f0caedeed5b7dca80e4e2257f6a6593811eb18ca 🚀

--- _next/static/WgdNJ1aKJm2KKhIfwdzlQ/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/428-06754d2df0c7b1e6a638.js","static/css/d023c363fe84e3657cf9.css","static/chunks/pages/index-2859442c5ab422aaba2f.js"],"/_error":["static/chunks/pages/_error-9faf4177fb4e528b4124.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
