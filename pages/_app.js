702a0c87d96d50fe4b8c5f412716b3379272f068
gabrielborbaweb <gabrielborba@protonmail.com>
2021-08-26 19:19:54 +0000

    snippet test

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,9 @@
 import '../styles/globals.css';
+import snippet from '../scripts/globals.js';
+
+window.onload = function() {
+  snippet();
+}
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
--- scripts/globals.js
+++ scripts/globals.js
@@ -1,6 +1,38 @@
-console.log("0 - iniciou")
+// console.log("0 - iniciou")
 
-window.onload = function() {
+// window.onload = function() {
+//     console.log("1 - load")
+    
+//     setTimeout(function() {
+//         console.log("2 - timeout")
+        
+//         function isInViewport(element) {
+//             console.log("3 - function")
+//             const rect = element.getBoundingClientRect();
+//             return (
+//                 rect.top >= 0 &&
+//                 rect.left >= 0 &&
+//                 rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+//                 rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+//             );
+//         }
+        
+//         let sections = document.querySelectorAll("section");
+        
+//         sections.forEach(section => {
+//             if (isInViewport(section)) {
+//                 section.classList.add("in-viewport");
+//             }
+//         });
+            
+//     }, 1000)
+        
+//     console.log("4 - final")
+// }
+// console.log("5 - final")
+
+
+const snippet = function() {
     console.log("1 - load")
     
     setTimeout(function() {
@@ -14,19 +46,21 @@ window.onload = function() {
                 rect.left >= 0 &&
                 rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
                 rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-                );
+            );
+        }
+        
+        let sections = document.querySelectorAll("section");
+        
+        sections.forEach(section => {
+            if (isInViewport(section)) {
+                section.classList.add("in-viewport");
             }
+        });
             
-            let sections = document.querySelectorAll("section");
-            
-            sections.forEach(section => {
-                if (isInViewport(section)) {
-                    section.classList.add("in-viewport");
-                }
-            });
-            
-        }, 1000)
+    }, 1000)
         
     console.log("4 - final")
 }
 console.log("5 - final")
+
+export default snippet
