56b9174a85753a532bfde051e944421fbcf6e4db
gabrielborbaweb <gabrielborba@protonmail.com>
2021-08-26 16:51:39 +0000

    test fix plasmic builder app

--- scripts/globals.js
+++ scripts/globals.js
@@ -1,21 +1,25 @@
 window.onload = function() {
-    
-    function isInViewport(element) {
-        const rect = element.getBoundingClientRect();
-        return (
-            rect.top >= 0 &&
-            rect.left >= 0 &&
-            rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-            rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-        );
-    }
-    
-    let sections = document.querySelectorAll("section");
-    
-    sections.forEach(section => {
-        if (isInViewport(section)) {
-            section.classList.add("in-viewport");
+
+    setTimeout(function() {
+        
+        function isInViewport(element) {
+            const rect = element.getBoundingClientRect();
+            return (
+                rect.top >= 0 &&
+                rect.left >= 0 &&
+                rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+                rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+            );
         }
-    });
+        
+        let sections = document.querySelectorAll("section");
+        
+        sections.forEach(section => {
+            if (isInViewport(section)) {
+                section.classList.add("in-viewport");
+            }
+        });
+
+    }, 1000)
 
 }
