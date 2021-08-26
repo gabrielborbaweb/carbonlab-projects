823e83ce33ea61e4c5c25ff2a013fa7647a1b850
gabrielborbaweb <gabrielborba@protonmail.com>
2021-08-26 15:27:16 +0000

    set animation trigger an styles

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import '../styles/globals.css'
+import '../scripts/globals.js'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
---
+++ scripts/global.js
@@ -0,0 +1,20 @@
+function isInViewport(element) {
+    const rect = element.getBoundingClientRect();
+    return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+}
+
+let sections = document.querySelectorAll("section")
+let arrows_right = document.querySelectorAll(".link svg")
+
+sections.forEach(section => {
+    if (isInViewport(section)) {
+        section.classList.add("in-viewport")
+        // let id = indexOf(section)
+        // arrows_right[id].classList.add("show")
+    }
+})
