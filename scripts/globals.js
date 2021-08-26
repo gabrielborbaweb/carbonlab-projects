4d9bfd54109ad9801633e7e813882bdafb9e38bf
gabrielborbaweb <gabrielborba@protonmail.com>
2021-08-26 19:28:12 +0000

    snippet test

--- scripts/globals.js
+++ scripts/globals.js
@@ -29,38 +29,38 @@
         
 //     console.log("4 - final")
 // }
+console.log("5 - glugs")
+
+
+// const snippet = function() {
+//     console.log("1 - load")
+    
+//     setTimeout(function() {
+//         console.log("2 - timeout")
+        
+//         function isInViewport(element) {
+//             console.log("3 - function")
+//             const rect = element.getBoundingClientRect();
+//             return (
+//                 rect.top >= 0 &&
+//                 rect.left >= 0 &&
+//                 rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+//                 rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+//             );
+//         }
+        
+//         let sections = document.querySelectorAll("section");
+        
+//         sections.forEach(section => {
+//             if (isInViewport(section)) {
+//                 section.classList.add("in-viewport");
+//             }
+//         });
+            
+//     }, 1000)
+        
+//     console.log("4 - final")
+// }
 // console.log("5 - final")
 
-
-const snippet = function() {
-    console.log("1 - load")
-    
-    setTimeout(function() {
-        console.log("2 - timeout")
-        
-        function isInViewport(element) {
-            console.log("3 - function")
-            const rect = element.getBoundingClientRect();
-            return (
-                rect.top >= 0 &&
-                rect.left >= 0 &&
-                rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-                rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-            );
-        }
-        
-        let sections = document.querySelectorAll("section");
-        
-        sections.forEach(section => {
-            if (isInViewport(section)) {
-                section.classList.add("in-viewport");
-            }
-        });
-            
-    }, 1000)
-        
-    console.log("4 - final")
-}
-console.log("5 - final")
-
-export default snippet
+// export default snippet
