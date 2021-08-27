c302a1230da9c0e244ccb316a7e495aad0f4358d
gabrielborbaweb <gabrielborba@protonmail.com>
2021-08-27 13:54:44 +0000

    snippet test

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,14 @@
 console.log("0 - app")
 
 import '../styles/globals.css';
-import snippet from '../scripts/globals.js';
+// import snippet from '../scripts/globals.js';
 
 console.log("1 - app")
 
 function MyApp({ Component, pageProps }) {
   return (<>
     <Component {...pageProps} />
-    {snippet()}
+    {/* {snippet()} */}
     </>)
 }
 
