25909761af561921fe595b5cec6206d378e0459b
gabrielborbaweb <gabrielborba@protonmail.com>
2021-08-27 12:17:04 +0000

    console test

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,15 @@
+console.log("0 - app")
+
 import '../styles/globals.css';
-import snippet from '../scripts/globals.js';
+
+console.log("1 - app")
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
 
-window.onload = function() {
-  snippet();
-}
+console.log("2 - app")
 
 export default MyApp
+
+console.log("3 - app")
