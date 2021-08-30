ed1a0c1b310bae1be4e52eedd6ec081085f1c08c
gabrielborbaweb <gabrielborba@protonmail.com>
2021-08-30 15:04:03 +0000

    form adjust]

--- components/ContactForm.js
+++ components/ContactForm.js
@@ -1,30 +1,7 @@
-// class ContactForm extends React.Component {
 const ContactForm = function() {
-  // constructor(props) {
-  //   super(props);
-  //   this.state = {
-  //     isGoing: true,
-  //     numberOfGuests: 2
-  //   };
 
-  //   this.handleChange = this.handleChange.bind(this);
-  // }
-
-  // handleChange(event) {
-  //   const target = event.target;
-  //   const name = target.name;
-
-  //   this.setState({
-  //     [name]: value
-  //   });
-  // }
-
-  // render() {
-    // return (
-  console.log("form");
   setTimeout(function() {
 
-    console.log("start form");
     document.querySelector("#form-block").innerHTML = 
       `<form>
         <label>
@@ -34,7 +11,6 @@ const ContactForm = function() {
             type="text"
           />
         </label>
-        <br />
         <label>
           Your Email:
           <input
@@ -42,7 +18,6 @@ const ContactForm = function() {
             type="mail"
           />
         </label>
-        <br />
         <label>
           Your Phone:
           <input
@@ -52,29 +27,22 @@ const ContactForm = function() {
         </label>
         <label>
           Message:
-          <textarea />
+          <textarea><textarea />
         </label>
         <input type="submit" value="SEND" />
       </form>`
-
-      console.log("end form");
       
       function openForm() {
-        console.log("function");
-        document.querySelector("form").classList.add("opened");
+        document.querySelector("#form-block").classList.add("opened");
       }
 
       document.querySelectorAll(".getintouch").forEach(btn => {
-        // btn.addEventListener("click", function() {
         btn.onclick = function() { 
-          console.log("click");
           openForm();
         };
       });
 
     }, 1000);
-    // )
-  // }
 }
 
 export default ContactForm
